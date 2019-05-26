38ecd1776a5337f7f8a67949193d564ad3497405
Ice5050 <ice50505050@hotmail.com>
2019-05-26 18:19:59 +0700

    [Added][1.6]

---
+++ 1.6.js
@@ -0,0 +1,19 @@
+// rotate :: [A] -> Int -> [A]
+// rotate arr from right to left n times
+function rotate(arr, n) {
+  if (n <= 0) return arr
+  else if (arr.length === 0) return arr
+  else {
+    return rotate(arr.slice(1).concat(arr[0]), n - 1)
+  }
+}
+
+// Test cases
+
+// Input: [1, 2, 3, 4, 5], 3
+// Output: [4, 5, 1, 2, 3]
+console.log(rotate([1, 2, 3, 4, 5], 3))
+
+// Input: ['a', 'b', 'c', 'd', 1, 2, 3], 10
+// Output: ['d', 1, 2, 3, 'a', 'b', 'c']
+console.log(rotate(['a', 'b', 'c', 'd', 1, 2, 3], 10))
